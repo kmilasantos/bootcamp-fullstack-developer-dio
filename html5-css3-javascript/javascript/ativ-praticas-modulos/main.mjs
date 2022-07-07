618777b9b8d9d67f9c8346096f84b62793e24523
kmilasantos <csilva.santos1997@gmail.com>
2022-07-07 14:08:50 -0400

    Atividade prática - Módulos

---
+++ html5-css3-javascript/javascript/ativ-praticas-modulos/main.mjs
@@ -0,0 +1,6 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Camila', 25));
+console.log(mostraCidade('Camila', 'Porto Velho'));
+console.log(mostraHobby('Camila','Assistir filmes e/ou séries'));
+
