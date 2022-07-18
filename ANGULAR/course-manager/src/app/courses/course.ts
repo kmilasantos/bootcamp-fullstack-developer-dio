8cb869777c4401d2cb927d397296aa1c44e4760d
kmilasantos <csilva.santos1997@gmail.com>
2022-07-18 15:01:39 -0400

    Primeiros passos com Angular

---
+++ ANGULAR/course-manager/src/app/courses/course.ts
@@ -0,0 +1,11 @@
+export class Course{
+    id: number | undefined;
+    name: string | undefined;
+    imageUrl: string | undefined;
+    price: number = 0;
+    code: string | undefined;
+    duration: number | undefined;
+    rating: number = 0;
+    releaseDate: string | undefined;
+    options: string | undefined;
+}
