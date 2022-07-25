67827c707516358e5ee0d5c4dad9aa9087d5d356
kmilasantos <csilva.santos1997@gmail.com>
2022-07-25 16:00:28 -0400

    Criando o componente users-form

--- ANGULAR/crud-dio-angular/src/app/app.module.ts
+++ ANGULAR/crud-dio-angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { UsersListComponent } from './pages/users/users-list/users-list.component';
+import { UsersFormComponent } from './pages/users/users-form/users-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UsersListComponent,
+    UsersFormComponent,
   ],
   imports: [
     BrowserModule,
